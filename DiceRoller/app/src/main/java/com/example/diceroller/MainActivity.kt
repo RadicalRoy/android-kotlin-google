6350f4d763e52e138ec0ce9e6637bbd36c5d2469
MathematicalYogi <rolandquintana1991@gmail.com>
2020-09-04 15:41:25 -0700

    diceroller updates, sunflower sample

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var leftDiceImage: ImageView
+    lateinit var rightDiceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        leftDiceImage = findViewById(R.id.left_dice_image)
+        rightDiceImage = findViewById(R.id.right_dice_image)
+
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
@@ -20,19 +27,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+
+        leftDiceImage.setImageResource(getImageResource())
+        rightDiceImage.setImageResource(getImageResource())
     }
 
     private fun countUp() {
-        val resultText:TextView = findViewById(R.id.result_text)
-        if(resultText.text.toString() === getString(R.string.hello_world)) {
-            resultText.text = 1.toString()
-        } else if (resultText.text.toString() == "6") {
-            return
-        } else {
-            resultText.text = (resultText.text.toString().toInt() + 1).toString()
+
+    }
+
+    private fun getImageResource(): Int {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 }
